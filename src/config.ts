ebca757c316ac4ebef40266362a88c00a73e1a45
fleet-mc <135062069+fleet-mc@users.noreply.github.com>
2024-06-20 02:42:29 -0700

    ip fix, i forgot about it being local

--- src/config.ts
+++ src/config.ts
@@ -46,7 +46,7 @@ export const config: Config = {
       originBlacklist: null,
     },
     server: {
-      host: "winterlegends.club",
+      host: "127.0.0.1",
       port: 25565,
     },
     tls: undefined,
